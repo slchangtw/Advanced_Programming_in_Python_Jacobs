3ae2a2b17a5e6c17dad95e1eecafd2e806cd4fc6
slchangtw <edi7709@gamil.com>
2018-05-01 14:10:47 +0200

    update calculator

--- assignment_4/calculator.py
+++ assignment_4/calculator.py
@@ -8,139 +8,84 @@
 
 from graphics import *
 
-class calculator(object):
-    """define a calculator"""
+def layout():
+    '''build the layout'''
+    # instruction
+    Text(Point(64, 10), "1. Enter two numbers").draw(win)
+    Text(Point(70, 20), "2. Press one operations").draw(win)
+    Text(Point(70, 180), "Click elsewhere to leave").draw(win)
+
+    # interface
+    Text(Point(50, 50), "First Number:").draw(win)
+    Text(Point(50, 70), "Second Number:").draw(win)
+
+    # result panel
+    shape = Rectangle(Point(0, 130), Point(200, 170))
+    shape.setFill('grey')
+    shape.setOutline('grey')
+    shape.draw(win)
     
-    def __init__(self):
-        self._layout()
-        self._first_num = ''
-        self._second_num = ''
-        self._operator = ''
-    
-    def _layout(self):
-
-        # print numbers
-        numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3]
-        for i in range(3):
-            for j in range(3):
-                shape = Rectangle(Point(20 + j * 40, 40 + i * 40), Point(50 + j * 40, 70 + i * 40))
-                shape.setFill("pink")
-                shape.draw(win)
-
-                text = Text(Point(35 + j * 40, 55 + i * 40), "{0}".format(numbers[j + i + 2 * i]))
-                text.draw(win)
-
-        # 0
-        shape = Rectangle(Point(20, 160), Point(50, 190))
+    # operation buttons
+    operators = ['+', '-', '*', '/']
+    for i in range(4):
+        shape = Rectangle(Point(10 + i * 30, 100), Point(30 + i * 30, 120))
         shape.setFill("pink")
         shape.draw(win)
 
-        text = Text(Point(35, 175), "0")
+        text = Text(Point(20 + i * 30, 110), "{0}".format(operators[i]))
         text.draw(win)
 
-        # print operators 
-        operators = ['+', '-', '*', '/', '=']
-        for i in range(5):
-            shape = Rectangle(Point(150, 40 + i * 30), Point(190, 60 + i * 30))
-            shape.setFill("pink")
-            shape.draw(win)
-
-            text = Text(Point(170, 50 + i * 30), "{0}".format(operators[i]))
-            text.draw(win)
-
-        # .
-        shape = Rectangle(Point(100, 170), Point(120, 180))
-        shape.setFill("pink")
-        shape.draw(win)
-
-        text = Text(Point(110, 175), ".")
-        text.draw(win)
-
-    def click(self, X_coord, Y_coord):
-        s = ''
-        if 20 < X_coord < 50 and 40 < Y_coord < 70:
-            s = '7'
-        if 60 < X_coord < 90 and 40 < Y_coord < 70:
-            s = '8'
-        if 100 < X_coord < 130 and 40 < Y_coord < 70:
-            s = '9'
-        if 20 < X_coord < 50 and 80 < Y_coord < 110:
-            s = '4' 
-        if 60 < X_coord < 90 and 80 < Y_coord < 110:
-            s = '5' 
-        if 100 < X_coord < 130 and 80 < Y_coord < 110:
-            s = '6' 
-        if 20 < X_coord < 50 and 120 < Y_coord < 150:
-            s = '1'
-        if 60 < X_coord < 90 and 120 < Y_coord < 150:
-            s = '2'
-        if 100 < X_coord < 130 and 120 < Y_coord < 150:
-            s = '3'
-        if 20 < X_coord < 50 and 160 < Y_coord < 190:
-            s = '0'
-        if 150 < X_coord < 190 and 40 < Y_coord < 60:
-            self._operator = '+'
-        if 150 < X_coord < 190 and 70 < Y_coord < 90:
-            self._operator = '-'
-        if 150 < X_coord < 190 and 100 < Y_coord < 120:
-            self._operator = '*'
-        if 150 < X_coord < 190 and 130 < Y_coord < 150:
-            self._operator = '/'
-        if 100 < X_coord < 120 and 170 < Y_coord < 180:
-            s = '.'
-        # equal sign
-        if 150 < X_coord < 190 and 160 < Y_coord < 180:
-            s = str(self._compute())
-        
-        # set first_num to inf if first_num is 'inf'
-        if self._first_num == 'inf':
-            self._first_num = ''
-            
-        if self._first_num == '' or self._operator == '':
-            self._first_num += s
+def compute(number_1, number_2, X_, Y_):
+    '''compute result'''
+    try:
+        if 10 < X_ < 30 and 100 < Y_ < 120:
+            return number_1 + number_2
+        if 40 < X_ < 60 and 100 < Y_ < 120:
+            return number_1 - number_2
+        if 70 < X_ < 90 and 100 < Y_ < 120:
+            return number_1 * number_2
+        if 100 < X_ < 120 and 100 < Y_ < 120:
+            return number_1 / number_2
         else:
-            self._second_num += s
-            
-        self._refresh_screen(self._first_num + self._operator + self._second_num)    
+            # leave if click elsewhere
+            win.close()
+    except ZeroDivisionError:
+        return 'inf'
+    except TypeError:
+        return 'invalid value'
+   
+def refresh_screen(s):
+    '''refresh result'''
+    
+    shape = Rectangle(Point(0, 130), Point(200, 170))
+    shape.setFill('grey')
+    shape.setOutline('grey')
+    shape.draw(win)
+    
+    Text(Point(100, 150), '{0}'.format(s)).draw(win)
 
-    def _refresh_screen(self,string):
-        shape = Rectangle(Point(10, 10), Point(180, 30))
-        shape.setFill("white")
-        shape.setOutline("white")
-        shape.draw(win)
-
-        text = Text(Point(100, 20), "{0}".format(string))
-        text.draw(win)
-        
-    def _compute(self):
-        num1 = float(self._first_num)
-        num2 = float(self._second_num)
-        
-        if self._operator == '+':
-            result = num1 + num2
-        if self._operator == '-':
-            result =  num1 - num2
-        if self._operator == '*':
-            result = num1 * num2
-        if self._operator == '/':
-            if num2 == 0: 
-                result = 'inf'
-            else:
-                result = num1 / num2
-        
-        self._first_num = ''
-        self._second_num = ''
-        self._operator = ''
-        
-        return result
-        
 if __name__ == '__main__':
+    # create layout
     win = GraphWin()
-    c = calculator()
+    layout()
+    entry_1 = Entry(Point(150, 50), 4)
+    entry_1.setText('0.0')
+    entry_1.draw(win)
+
+    entry_2 = Entry(Point(150, 70), 4)
+    entry_2.setText("0.0")
+    entry_2.draw(win)
+    
     while True:
         p = win.getMouse()
-        X_, Y_ = p.getX(), p.getY()
-        c.click(X_, Y_)
-    win.close()
-    
-    
+        try:
+            num_1 = eval(entry_1.getText())
+            num_2 = eval(entry_2.getText())
+            X_, Y_ = p.getX(), p.getY()
+            s = compute(num_1, num_2, X_, Y_)
+        except SyntaxError:
+            s = 'invalid value'
+        finally:
+            refresh_screen(s)
+            
+  
